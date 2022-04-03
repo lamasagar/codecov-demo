c75921336fafe2ca5344c34ab55ea2ba74495238
sagarPakhrin <sagarlama826@gmail.com>
2022-04-03 23:44:05 +0545

    Statement coverage

--- libs/codecov-demo/src/lib/codecov-demo.spec.ts
+++ libs/codecov-demo/src/lib/codecov-demo.spec.ts
@@ -8,4 +8,9 @@ describe('codecovDemo', () => {
     printGreaterNumber(5, 10);
     expect(log).toHaveBeenCalledWith(10);
   });
+
+  it('should print 10 when 10,5 is passed', () => {
+    printGreaterNumber(10, 5);
+    expect(log).toHaveBeenCalledWith(10);
+  });
 });
